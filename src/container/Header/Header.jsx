eda320529ad400538f18a0ad5b7aa8c9f300e4dd
Shane <shaneboo152@gmail.com>
2022-08-06 14:51:27 -0700

    testing

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -19,10 +19,13 @@ const Header = () => {
 
   return (
     <div className="bg-black ">
-    <div className="flex items-center justify-between h-screen  section__padding app__header app__header bg-left" id="home">
-      {/* <Waypoint onEnter={() => setRenderLottie(true)} />
+      <div
+        className="flex items-center justify-between h-screen section__padding app__header "
+        id="home"
+      >
+        {/* <Waypoint onEnter={() => setRenderLottie(true)} />
       <Waypoint onLeave={() => setRenderLottie(false)} /> */}
-      {/* <div className="app__wrapper_img lg:pt-12 mr-[200px]  ">
+        {/* <div className="app__wrapper_img lg:pt-12 mr-[200px]  ">
         <img
           src={images.dinner}
           alt="header_image"
@@ -30,44 +33,44 @@ const Header = () => {
         />
       </div> */}
 
-      <div className="app__wrapper_info relative  ">
-        <div class="space-y-7  ">
-          <SubHeading title={"Chase The New Flavor"} />
-          <div className="lg:w-[70%]">
-            <h1 className="section-title">The Key To Fine Dining</h1>
-          </div>
-          <div className="lg:w-[70%]">
-            <p className="text-color_white mb-9 font-openSans leading-8">
-              Sit tellus lobortis sed senectus vivamus molestie. Condimentum
-              volutpat morbi facilisis quam scelerisque sapien. Et, penatibus
-              aliquam amet tellus
-            </p>
-          </div>
-          <a
-            href="#menu"
-            class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
-          >
-            <span
-              class="w-48 h-48 rounded rotate-[-40deg] bg-color_golden
+        <div className="app__wrapper_info relative  ">
+          <div class="space-y-7  ">
+            <SubHeading title={"Chase The New Flavor"} />
+            <div className="lg:w-[70%]">
+              <h1 className="section-title">The Key To Fine Dining</h1>
+            </div>
+            <div className="lg:w-[70%]">
+              <p className="text-color_white mb-9 font-openSans leading-8">
+                Sit tellus lobortis sed senectus vivamus molestie. Condimentum
+                volutpat morbi facilisis quam scelerisque sapien. Et, penatibus
+                aliquam amet tellus
+              </p>
+            </div>
+            <a
+              href="#menu"
+              class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
+            >
+              <span
+                class="w-48 h-48 rounded rotate-[-40deg] bg-color_golden
          absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all
           translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 
           group-hover:translate-x-0"
-            ></span>
-            <span
-              class="font-cormorant relative w-full text-left text-black 
+              ></span>
+              <span
+                class="font-cormorant relative w-full text-left text-black 
         transition-colors duration-300 ease-in-out group-hover:text-white text-lg font-bold"
-            >
-              Explore Menu
-            </span>
-          </a>
-          {/* {renderLottie && (
+              >
+                Explore Menu
+              </span>
+            </a>
+            {/* {renderLottie && (
             <div className="hidden lg:flex absolute top-1 right-5">
               <Lottie animationData={animation} loop={1} />
             </div>
           )} */}
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
