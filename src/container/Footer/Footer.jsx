7610788475f2f0ae834b909962c75317af057176
Shane <shaneboo152@gmail.com>
2022-08-11 00:25:06 -0700

    development

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => { return(
-  <div className='flex section__padding'>
+  <div className='flex flex-col app__bg h-screen'>
+    <div className='bg-color_black h-[30%]'></div>
     
   </div>
 );
