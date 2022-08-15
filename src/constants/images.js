b754c998d804db7b23cd7652362866a22b707d6e
Shane <shaneboo152@gmail.com>
2022-08-15 00:59:39 -0700

    change text color in heading

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,38 +1,38 @@
-import bg from '../assets/bg.png';
-import chef from '../assets/chef.png';
-import G from '../assets/G.png';
-import gallery01 from '../assets/gallery01.png';
-import gallery02 from '../assets/gallery02.png';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
-import knife from '../assets/knife.png';
-import logo from '../assets/logo.png';
-import menu from '../assets/menu.png';
-import overlaybg from '../assets/overlaybg.png';
-import spoon_left from '../assets/spoon-left.png';
-import spoon_right from '../assets/spoon-right.png';
-import welcome from '../assets/welcome.png';
-import findus from '../assets/findus.png';
-import laurels from '../assets/laurels.png';
-import award01 from '../assets/award01.png';
-import award02 from '../assets/award02.png';
-import award03 from '../assets/award03.png';
-import award05 from '../assets/award05.png';
-import sign from '../assets/sign.png';
-import quote from '../assets/quote.png';
-import gericht from '../assets/gericht.png';
+import bg from "../assets/bg.png";
+import chef from "../assets/chef.png";
+import G from "../assets/G.png";
+import gallery01 from "../assets/gallery01.png";
+import gallery02 from "../assets/gallery02.png";
+import gallery03 from "../assets/gallery03.png";
+import gallery04 from "../assets/gallery04.png";
+import knife from "../assets/knife.png";
+import logo from "../assets/logo.png";
+import menu from "../assets/menu.png";
+import overlaybg from "../assets/overlaybg.png";
+import spoon_left from "../assets/spoon-left.png";
+import spoon_right from "../assets/spoon-right.png";
+import welcome from "../assets/welcome.png";
+import findus from "../assets/findus.png";
+import laurels from "../assets/laurels.png";
+import award01 from "../assets/award01.png";
+import award02 from "../assets/award02.png";
+import award03 from "../assets/award03.png";
+import award05 from "../assets/award05.png";
+import sign from "../assets/sign.png";
+import quote from "../assets/quote.png";
+import gericht from "../assets/gericht.png";
 import steak from "../assets/Pan-Seared-Steak-4.jpg";
-import menu_dark from "../assets/menu-dark.jpg"
-import restaurant_background from "../assets/restaurant-background.jpg"
-import cocktail from "../assets/cocktail.jpg"
-import dinner from "../assets/dinner.jpg"
-import drink from "../assets/drink.webp"
-import about from "../assets/about-us.jpg"
-import dessert from "../assets/dessert.jpg"
-import slider_three from "../assets/about-slider-three.webp"
-import slider_two from "../assets/about-us-slide-two.jpg"
+import menu_dark from "../assets/menu-dark.jpg";
+import restaurant_background from "../assets/restaurant-background.jpg";
+import cocktail from "../assets/cocktail.jpg";
+import dinner from "../assets/dinner.jpg";
+import drink from "../assets/drink.webp";
+import about from "../assets/about-us.jpg";
+import dessert from "../assets/dessert.jpg";
+import slider_three from "../assets/about-slider-three.webp";
+import slider_two from "../assets/about-us-slide-two.jpg";
 
-export default {
+const images = {
   slider_two,
   slider_three,
   dessert,
@@ -67,3 +67,5 @@ export default {
   quote,
   gericht,
 };
+
+export default images;
