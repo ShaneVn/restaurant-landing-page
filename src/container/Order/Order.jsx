bece3926ac3597e3051fb65ff209640f10110369
Shane <shaneboo152@gmail.com>
2022-08-16 00:20:28 -0700

    finish the header for order page

--- src/container/Order/Order.jsx
+++ src/container/Order/Order.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Order.css";
+import { Button } from "../../components";
 
 function Order() {
   return (
-    <div className="h-screen w-full order__header" id="order_home">
-      
-   
+    <div className="h-screen w-full order__header flex items-center justify-center " id="order_home">
+      <div className="section__padding  ">
+        <h1 className="section-title text-color_crimson border-b-[7px] border-r-4 pr-5 lg:pr-[80px] border-color_golden mb-12">Best Food In Town <br/>  Order Now!</h1>
+        
+        <Button text="Place Order"/>
+        </div>
+
+
     </div>
   );
 }
