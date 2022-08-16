1ee505a2e873a3f8623c8543c0e5ca61b86b4950
Shane <shaneboo152@gmail.com>
2022-08-16 00:23:08 -0700

    add animation for text

--- src/container/Order/Order.jsx
+++ src/container/Order/Order.jsx
@@ -4,14 +4,23 @@ import { Button } from "../../components";
 
 function Order() {
   return (
-    <div className="h-screen w-full order__header flex items-center justify-center " id="order_home">
-      <div className="section__padding  ">
-        <h1 className="section-title text-color_crimson border-b-[7px] border-r-4 pr-5 lg:pr-[80px] border-color_golden mb-12">Best Food In Town <br/>  Order Now!</h1>
-        
-        <Button text="Place Order"/>
-        </div>
+    <div
+      className="h-screen w-full order__header flex items-center justify-center "
+      id="order_home"
+    >
+      <div
+        className="section__padding"
+        data-aos="fade-down"
+        data-aos-duration="1200"
+        data-aos-easing="ease-in-out"
+        data-aos-mirror="true"
+      >
+        <h1 className="section-title text-color_crimson border-b-[7px] border-r-4 pr-5 lg:pr-[80px] border-color_golden mb-12">
+          Best Food In Town <br /> Order Now!
+        </h1>
 
-
+        <Button text="Place Order" />
+      </div>
     </div>
   );
 }
